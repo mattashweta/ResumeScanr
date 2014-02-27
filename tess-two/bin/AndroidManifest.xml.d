5637cc40db79d7e4324f216226d270d979407552
Ketan <ketanakade@gmail.com>
2014-02-27 11:52:49 -0800

    Added starting project and supporting  tessTwo library

---
+++ tess-two/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+E:\coursera\TEsstwo\tess-two-master\tess-two\bin\AndroidManifest.xml : \
+E:\coursera\TEsstwo\tess-two-master\tess-two\AndroidManifest.xml \
---
+++ tess-two/bin/classes/R.java.d
@@ -0,0 +1,1 @@
+ : E:\coursera\TEsstwo\tess-two-master\tess-two\bin\AndroidManifest.xml \
