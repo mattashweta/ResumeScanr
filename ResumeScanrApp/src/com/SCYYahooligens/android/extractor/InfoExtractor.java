b924a7c5986cc1d937d0f8d5a0b63d7b0463f1e8
Shashank Mandil <shashankm@yahoo-inc.com>
2014-02-27 20:50:33 -0800

    Deleted the classifiers and standford ner from git repo

---
+++ ResumeScanrApp/src/com/SCYYahooligens/android/extractor/InfoExtractor.java
@@ -0,0 +1,16 @@
+package com.SCYYahooligens.android.extractor;
+
+public class InfoExtractor {
+	private Resume instance;
+	
+	public InfoExtractor()
+	{
+		instance = new Resume();
+	}
+	
+	public Resume getInfo(String s)
+	{
+		
+		return instance;
+	}
+}
---
+++ ResumeScanrApp/src/com/SCYYahooligens/android/extractor/Resume.java
@@ -0,0 +1,18 @@
+package com.SCYYahooligens.android.extractor;
+
+public class Resume {
+	public String studName;
+	public String studGPA;
+	public String studEmail;
+	public String studBranch;
+	public String studUniv;
+	
+	Resume()
+	{
+		studName = null;
+		studGPA = null;
+		studEmail = null;
+		studBranch = null;
+		studUniv = null;
+	}
+}
