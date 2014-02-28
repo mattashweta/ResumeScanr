a7dee3895cb90e6b0b4c2d47c68c64565f3b531a
Ketan <ketanakade@gmail.com>
2014-02-27 21:04:49 -0800

    Added running tess-two project code

--- tess-two/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\coursera\TEsstwo\tess-two-master\tess-two\bin\AndroidManifest.xml : \
-E:\coursera\TEsstwo\tess-two-master\tess-two\AndroidManifest.xml \
--- tess-two/bin/classes/R.java.d
+++
@@ -1,1 +0,0 @@
- : E:\coursera\TEsstwo\tess-two-master\tess-two\bin\AndroidManifest.xml \
